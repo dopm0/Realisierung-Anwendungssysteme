2c0400fff489401f3b08b0287ef259ee1cffcb22
JuliaPfuetzer <j.pfuetzer@gmail.com>
2025-05-18 21:28:02 +0200

    URL verbessert und Login auskommentiert

--- energyWebapp/apps/baseApp/routes.py
+++ energyWebapp/apps/baseApp/routes.py
@@ -8,7 +8,7 @@ base = Blueprint('base', __name__, template_folder='templates')
 # Home Redirect
 @base.route('/', methods=['GET'])
 def index():
-    return redirect(url_for('base.login_view1'))
+    return redirect(url_for('base.home_view'))
 
 # Home Ansicht
 @base.route('/home', methods=['GET'])
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from energyWebapp.apps.batteryApp.routes import battery
 def create_app():
     appl = Flask(__name__, static_folder='Apps/baseApp/static', static_url_path='/base')
     appl.register_blueprint(base, url_prefix='/')
-    appl.register_blueprint(history, url_prefix='/history')
-    appl.register_blueprint(battery, url_prefix='/battery')
+    appl.register_blueprint(history, url_prefix='/')
+    appl.register_blueprint(battery, url_prefix='/')
     return appl
 
 if __name__ == '__main__':
