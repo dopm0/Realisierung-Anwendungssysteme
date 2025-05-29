4f07c9e10e65a9332606b117352310c6bf7c8672
dopm0 <kai.droege@pm.me>
2025-05-29 23:57:43 +0200

    Finale Version

--- init.py
+++
@@ -1,8 +0,0 @@
-from main import create_app
-from energyWebapp.general.db.extensions import db  # falls du extensions.py verwendest
-
-app = create_app()
-
-with app.app_context():
-    db.create_all()
-    print("User-Tabelle erfolgreich im Schema 'mw212_projekt' erstellt.")
